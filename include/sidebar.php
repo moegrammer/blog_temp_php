1e6d0fc073128f2431fbd3ec08034b13af71c82d
Adam Mawlawi <amawlawi@wwmach.com>
2017-07-23 14:29:43 -0500

    working on search

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,18 +1,32 @@
             <div class="col-md-4">
 
+
+
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
+                        <?php
+                        if(isset($_POST['submit'])){
+                            echo $searach = $_POST['search'];
+                        }
+
+                        ?>
+
+                    <form action="" method="post">
+                        <div class="input-group">
+                            <input type="text" name="search" class="form-control">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="submit" name="submit">
+                                    <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                            </span>
+                        </div>
+                    </form>
                     <!-- /.input-group -->
                 </div>
+
+
+
 
                 <!-- Blog Categories Well -->
                 <div class="well">
